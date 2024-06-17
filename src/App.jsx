96214d5636443b807d850722980bc90a35b1d339
ylprast <152499309+ylprast@users.noreply.github.com>
2024-06-17 21:02:09 +0700

    updating app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import { Routes, Route, BrowserRouter } from "react-router-dom"
-import HomePage from "./pages/home"
-import FAQPage from "./pages/faq"
-import ProductPage from "./pages/product"
-import AboutUsPage from "./pages/about-us"
-import RegisterPage from './pages/register';
-import LoginPage from './pages/login';
+import HomePage from "./pages/home.jsx"
+import FAQPage from "./pages/faq.jsx"
+import ProductPage from "./pages/product.jsx"
+import AboutUsPage from "./pages/about-us.jsx"
+import RegisterPage from './pages/register.jsx';
+import LoginPage from './pages/login.jsx';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
@@ -15,43 +15,43 @@ import Nav from 'react-bootstrap/Nav';
 function App() {
 
   return (
+    <>
     <nav className="navbar" >
 <BrowserRouter>
 
       <div className="container navbar-nav  ms-lg-auto">
-        <Nav fill variant="tabs" defaultActiveKey=" ">
-        <ul>
-    <Nav.Item>
-        <Nav.Link to="/" element={<HomePage />}>Home</Nav.Link>
+        <Nav fill variant="tabs" defaultActiveKey="/">
+  <Nav.Item>
+        <Nav.Link href="/" element={<HomePage />}>Home</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link to="./pages/faq" element={<FAQPage />} >FAQ</Nav.Link>
+        <Nav.Link href="./pages/faq" element={<FAQPage />} >FAQ</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link to="./pages/about-us" element={<AboutUsPage />}>About Us</Nav.Link>
+        <Nav.Link href="./pages/about-us" element={<AboutUsPage />}>About Us</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link to="./pages/product-detail" element={<ProductPage />}>Product</Nav.Link>
+        <Nav.Link href="./pages/product-detail" element={<ProductPage />}>Product</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link to="./pages/register" element={<RegisterPage />}>Register</Nav.Link>
+        <Nav.Link href="./pages/register" element={<RegisterPage />}>Register</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link to="./pages/login" element={<LoginPage />}>Login</Nav.Link>
+        <Nav.Link href="./pages/login" element={<LoginPage />}>Login</Nav.Link>
       </Nav.Item>
-</ul>
     </Nav>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="./pages/faq" element={<FAQPage />} />
-          <Route path="./pages/about-us" element={<AboutUsPage />}></Route>
-          <Route path="./pages/product-detail" element={<ProductPage />}></Route>
-          <Route path="./pages/register" element={<RegisterPage />} />
-          <Route path="./pages/login" element={<LoginPage />} />
+          <Route path="faq" element={<FAQPage />} />
+          <Route path="about-us" element={<AboutUsPage />}></Route>
+          <Route path="product-detail" element={<ProductPage />}></Route>
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="login" element={<LoginPage />} />
         </Routes>
       </div>
     </BrowserRouter>
     </nav>
+    </>
   )
 }
 
